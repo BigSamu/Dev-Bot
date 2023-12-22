19ca06d6d94fd5fe492e03bf31f1aaadcbe63eda
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-12-22 10:42:02 -0300

    adding export for function setupWebhooks for testing purposes
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- config/webhooks.js
+++ config/webhooks.js
@@ -1,6 +1,6 @@
 import { createOrUpdateChangesetFile } from "../changeset-creator/index.js";
 
-const setupWebhooks = (ghApp) => {
+export const setupWebhooks = (ghApp) => {
   ghApp.webhooks.on("pull_request.created", async ({ octokit, payload }) => {
     console.log(
       `Received a pull request creation event for #${payload.pull_request.number}`
