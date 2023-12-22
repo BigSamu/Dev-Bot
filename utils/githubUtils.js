447175e44189884301779c151192b4b4a8bbf06a
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-12-22 10:59:39 -0300

    updating JS Documentation for extractPullRequestData util function
    Signed-off-by: Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>

--- utils/githubUtils.js
+++ utils/githubUtils.js
@@ -12,10 +12,23 @@ import {
 import { SKIP_LABEL } from "../config/constants.js";
 
 /**
- * Extracts relevant data from a GitHub Pull Request from the GitHub action context.
+ * Extracts relevant data from a GitHub Pull Request provided as a payload. This function
+ * is typically used within the context of a GitHub Action to process pull request information.
  *
- * @returns {Object} Object containing pull request details.
- * @throws {PullRequestDataExtractionError} If data extraction fails.
+ * @param {Object} pr_payload - The payload object from a GitHub pull request event.
+ * @param {Object} pr_payload.base - The base object containing the target branch info.
+ * @param {Object} pr_payload.head - The head object containing the source branch info.
+ * @param {number} pr_payload.number - The number of the pull request.
+ * @param {string} pr_payload.body - The description or body of the pull request.
+ * @param {string} pr_payload.html_url - The HTML URL link to the pull request.
+ *
+ * @returns {Object} An object containing essential pull request details such as repository owner,
+ * repository name, base branch reference, head repository owner, head repository name, head branch reference,
+ * pull request number, description, and pull request link.
+ *
+ * @throws {PullRequestDataExtractionError} If data extraction from the payload fails, a custom error
+ * `PullRequestDataExtractionError` is thrown. This helps in isolating issues specific to data extraction
+ * process in the action's workflow.
  */
 export const extractPullRequestData = (pr_payload) => {
   try {
