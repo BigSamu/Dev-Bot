5cc5a102571f0c83d2831071ddf45f6abe9482e0
Johnathon Bowers <johnathonbowers@gmail.com>
2023-12-30 20:50:15 -0500

    Add minor typographical edits
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/webhooks/github.webhooks.js
+++ src/webhooks/github.webhooks.js
@@ -4,20 +4,19 @@ export const setupWebhooks = (webhooks,webhookUrl) => {
 
   webhooks.on("pull_request.opened", async ({ payload }) => {
     console.log(
-      `Received a pull request creation event for #${payload.pull_request.number}`
+      `Received a "pull request opened" event for #${payload.pull_request.number}`
     );
     await createOrUpdateChangesetFile(payload);
   });
 
   webhooks.on("pull_request.edited", async ({ payload }) => {
     console.log(
-      `Received a pull request edition event for #${payload.pull_request.number}`
+      `Received a "pull request edited" event for #${payload.pull_request.number}`
     );
     await createOrUpdateChangesetFile(payload);
   });
 
   webhooks.onError((error) => {
-    // Error handling logic
     console.error("Error handling webhook:", error);
   });
 };
