36f213d48c6b55695b8fa2b6235090d08c0d8bd0
Johnathon Bowers <johnathonbowers@gmail.com>
2023-12-31 14:22:51 -0500

    Remove unused `ENVIRONMENT` variable
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-export const ENVIRONMENT = process.env.NODE_ENV;
 export const PORT = process.env.PORT || 3000;
 export const API_PATH_SUFFIX = "/api/v1";
 
