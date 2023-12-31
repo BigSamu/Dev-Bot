901e4ba36445374668a665d9131973178aac4d57
Johnathon Bowers <johnathonbowers@gmail.com>
2023-12-30 20:39:19 -0500

    Update JSDoc comments
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/utils/pullRequest.utils.js
+++ src/utils/pullRequest.utils.js
@@ -3,15 +3,10 @@
  * is typically used within the context of a GitHub Action to process pull request information.
  *
  * @param {Object} pr_payload - The payload object from a GitHub pull request event.
- * @param {Object} pr_payload.base - The base object containing the target branch info.
- * @param {Object} pr_payload.head - The head object containing the source branch info.
- * @param {number} pr_payload.number - The number of the pull request.
- * @param {string} pr_payload.body - The description or body of the pull request.
- * @param {string} pr_payload.html_url - The HTML URL link to the pull request.
  *
- * @returns {Object} An object containing essential pull request details such as repository owner,
+ * @returns {Object} An object containing essential pull request details such as base repository owner, base 
  * repository name, base branch reference, head repository owner, head repository name, head branch reference,
- * pull request number, description, and pull request link.
+ * pull request number, pull request description, and pull request link.
  *
  * @throws {PullRequestDataExtractionError} If data extraction from the payload fails, a custom error
  * `PullRequestDataExtractionError` is thrown. This helps in isolating issues specific to data extraction
